b4b1e6f111d752664951c22a88448b9b4a374a04
gianluca <g.contaldi@dstech.it>
2017-10-23 16:16:23 +0200

    added UserDao

---
+++ src/main/java/it/relatech/dao/UserDaoImpl.java
@@ -0,0 +1,52 @@
+package it.relatech.dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+import it.relatech.model.User;
+
+@Repository
+public class UserDaoImpl extends AbstractDao implements UserDao {
+
+	@Override
+	public User saveUser(User user) {
+		persist(user);
+		return user;
+	}
+
+	@Override
+	public User getUserById(int id) {
+		Criteria criteria = getSession().createCriteria(User.class);
+		criteria.add(Restrictions.eq("id", id));
+		return (User) criteria.uniqueResult();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<User> getListUsers() {
+		return getSession().createCriteria(User.class).list();
+	}
+
+	@Override
+	public User getUserByUsernameAndPassword(String username, String password) {
+		Criteria criteria = getSession().createCriteria(User.class);
+		criteria.add(Restrictions.eq("username", username));
+		criteria.add(Restrictions.eq("password", password));
+		return (User) criteria.uniqueResult();
+	}
+
+	@Override
+	public User updateUser(User user) {
+		update(user);
+		return null;
+	}
+
+	@Override
+	public void deleteUserById(int id) {
+		delete(getUserById(id));
+	}
+
+}
